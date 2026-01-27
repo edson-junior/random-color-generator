83541130ff49c1d8ebcaf7a01cbdacf05e752c0f
Edson Junior <soneddjr@gmail.com>
2026-01-27 16:36:27 +0100

    Run prettier on index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-const hue = process.argv[2]
-const luminosity = process.argv[3]
+const hue = process.argv[2];
+const luminosity = process.argv[3];
 const color = randomColor({ hue, luminosity });
 const colorOutput = chalk.hex(color);
 
 // console.log({ hue, luminosity, color })
 
-console.log(colorOutput(`
+console.log(
+  colorOutput(`
 ###############################
 ###############################
 ###############################
@@ -18,4 +19,5 @@ console.log(colorOutput(`
 ###############################
 ###############################
 ###############################
-`));
+`),
+);
