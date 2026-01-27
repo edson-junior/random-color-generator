ca901d24a39a59bccce87c0a9fbda22d5ca688d5
Edson Junior <soneddjr@gmail.com>
2026-01-27 15:47:39 +0100

    Create random color generator

---
+++ index.js
@@ -0,0 +1,21 @@
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const hue = process.argv[2]
+const luminosity = process.argv[3]
+const color = randomColor({ hue, luminosity });
+const colorOutput = chalk.hex(color);
+
+// console.log({ hue, luminosity, color })
+
+console.log(colorOutput(`
+###############################
+###############################
+###############################
+#####                     #####
+#####       ${color}       #####
+#####                     #####
+###############################
+###############################
+###############################
+`));
